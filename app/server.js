2ff555dce47737e4f3da98befd2ee9ccd2c62355
woong <wooong1225@gmail.com>
2018-03-21 15:04:02 -0700

    Add database authentication code on server

--- app/server.js
+++ app/server.js
@@ -2,31 +2,38 @@ const express = require('express')
 const path = require('path')
 const bodyParser = require('body-parser')
 const app = express()
-
-const Web3 = require('web3')
-
-if (typeof web3 != 'undefined') {
-  var web3 = new Web3(Web3.currentProvider)
-} else {
-  var web3 = new Web3(new Web3.providers.WebsocketProvider('ws://localhost:8545'))
-}
-
-console.log(web3.eth.accounts.create())
+const db = require('./models')
 
 app.use(express.static('public'))
 app.use(bodyParser.json({type: 'application/*+json'})) // for pasing application/json
-app.use(bodyParser.urlencoded({extended: true})); // for parsing application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true})) // for parsing application/x-www-form-urlencoded
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'public/home.html'))
 })
 
+// const Web3 = require('web3')
+
+// if (typeof web3 != 'undefined') {
+//   var web3 = new Web3(Web3.currentProvider)
+// } else {
+//   var web3 = new Web3(new Web3.providers.WebsocketProvider('ws://localhost:8545'))
+// }
+
+// console.log(web3.eth.accounts.create())
+
 app.post('/wallet/create', function (req, res) {
+
   // TODO: make new wallet and save user data on database(username,licenseNumber,wallet address, privatekey)
   //   req.body.username, req.body.licenseNumber
-  var newWallet = web3.eth.accounts.create();
+  var newWallet = web3.eth.accounts.create()
 })
 
 app.listen(3000, function () {
   console.log('Toy App listening on port 3000')
-});
+  db.sequelize.authenticate().then(function () { // NOTE : Need to get used to Promise Statement
+    console.log('Database connected')
+  }).catch(function (err) {
+    console.log(err)
+  }).done()
+})
