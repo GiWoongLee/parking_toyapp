72c338ade4d58ff255fd7aa78c5ce78a07b70449
woong <wooong1225@gmail.com>
2018-03-22 15:26:16 -0700

    Hard coded user related info & [Bug] register() not working

--- app/public/js/app.js
+++ app/public/js/app.js
@@ -1,24 +1,32 @@
-var tempUserName = "Clay"
-var tempLicenseNumber = "2CJC569"
+var tempUserName = 'giwoong'
+var tempLicenseNumber = '2CJC569'
+var tempPassword = 'tempPWD'
 
-var register = function(){
-    var registration = $("#registration")
-    registration.on("click",function(event){
-        $.ajax({
-            method: "POST",
-            url: "/waleet/create",
-            data : {
-                username : tempUserName, 
-                licenseNumber : tempLicenseNumber
-            },
-            success : function(data){
-                console.log("Successful registration")
-            },
-            error : function(data){
-                console.log("Error making new account")
-            }
-        })
+var register = function () {
+  $('#registration').on('click', function (event) {
+    $.ajax({
+      method: 'POST',
+      url: '/account/create',
+      data: {
+        username: tempUserName,
+        licenseNumber: tempLicenseNumber,
+        password: tempPassword
+      },
+      success: function (data) {
+        console.log('Successful registration')
+        // TODO : Make a file to download EncryptedPrivateKey
+        // TODO : We need to handle this issue - User might need to write EncryptedPrivateKey everytime
+        console.log(data.privateKey)
+      },
+      error: function (error) {
+        console.log('Error making new account')
+        console.log(error)
+      }
     })
+  })
 }
 
-register()
+// TODO : Refactoring. Need to come up with better solution than document.ready
+$(document).ready(function (e) {
+  register()
+})
