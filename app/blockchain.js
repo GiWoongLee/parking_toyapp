b3a129adc6b4c7cb439c708794fd53e38c60b1f1
woong <wooong1225@gmail.com>
2018-04-10 16:22:22 -0700

    Implement createWallet and payment from one to another

--- app/blockchain.js
+++ app/blockchain.js
@@ -1,4 +1,7 @@
 const Web3 = require('web3')
+const bip39 = require('bip39')
+const hdkey = require('ethereumjs-wallet/hdkey')
+const utils = require('ethereumjs-util')
 
 if (typeof web3 != 'undefined') {
   var web3 = new Web3(Web3.currentProvider)
@@ -100,9 +103,9 @@ var payment = function (paymentInfo) { // TODO : Replace parameters with relevan
       return Promise.all(accounts.map(account => getBalance(account.address))) // STEP 2 : check balances of sender/receiver before transaction
         .then(() => transferEther(accounts[0], rawTx)) // STEP 3 : transferEther from sender to receiver
         .then(() => accounts.map(account => getBalance(account.address))) // STEP 4 : check balances of sender/receiver after transaction
-        .catch(function(error){console.log(error)})
+        .catch(function (error) {console.log(error)})
     })
-    .catch(function(error){
+    .catch(function (error) {
       console.log(error)
       return error
     })
@@ -110,33 +113,50 @@ var payment = function (paymentInfo) { // TODO : Replace parameters with relevan
 
 module.exports = {
   showAllAccounts: showAllAccounts,
-  encryptPvKey : encryptPvKey,
+  encryptPvKey: encryptPvKey,
   registration: registration,
   payment: payment
 }
 
-// TODO : (Ethereum-hdkey/bip39)Test making new wallet and account for user in ganache
 
-// TODO: (ethereumhdkey-bip39/rpc call related library)Test making transaction from one account to another in ganache
+//TODO : remove fixture
+var paymentInfo = {
+  pvKeys: { // hardcoded private keys 
+    sender: '0x32d4e4b8deae4967f6ec305683921b1d46517767ef7a3411c27bbcf24fa7b757',
+    receiver: '0x90e40b307bd5ee5c7f5285aecffcf0fb223ff1cf802d913237ecaf2f962e251e'
+  },
+  txInfo: {
+    gasPrice: '200', // string
+    gas: '210000', // string
+    value: '1000', // string
+    data: '' // string
+  }
+}
 
-// var mnemonic = bip39.generateMnemonic() var seed =
-// bip39.mnemonicToSeed(mnemonic) var hdwallet = hdkey.fromMasterSeed(seed) var
-// hdwalletPath = "m/44'/60'/0/0" var wallet =
-// hdwallet.derivePath(hdwalletPath).getWallet() // NOTE : create wallet var
-// address = '0x' + wallet.getAddress().toString('hex') console.log(address)
-// web3.utils.isAddress(address) var account =
-// web3.eth.accounts.privateKeyToAccount(wallet.getPrivateKey())
-// console.log(account) app.post('/wallet/create', function (req, res) {   //
-// NOTE : exeucting web3, node is running on the platform/network interacting
-// with other nodes   // NOTE : ganache makes several accounts under wallet to
-// help development/test   var mnemonic = bip39.generateMnemonic()   var seed =
-// bip39.mnemonicToSeed(mnemonic)   var hdwallet = hdkey.fromMasterSeed(seed)
-// var hdwalletPath = "m/44'/60'/0/0"   var wallet =
-// hdwallet.derivePath(hdwalletPath).getWallet() // NOTE : create wallet   var
-// address = '0x' + wallet.getAddress().toString('hex')   console.log(address)
-// }) app.get('/wallet/(:id)', function (req, res) { // TODO : add user_id,
-// account_id, including database modeling   var seed =
-// bip39.mnemonicToSeed(mnemonic)   var hdwallet = hdkey.fromMasterSeed(seed)
-// var hdwalletPath = "m/44'/60'/0/0"   var wallet =
-// hdwallet.derivePath(hdwalletPath).getWallet() // NOTE : create wallet   var
-// address = '0x' + wallet.getAddress().toString('hex') })
+var loadGanacheWallet = function () { // NOTE : wallet from ganache 10 accounts
+  var mnemonic = 'humor tragic easy wing rack silk diesel suit cloth alley unknown wine'
+  var path = "m/44'/60'/0'/0/0" // first account
+  var seed = bip39.mnemonicToSeed(mnemonic)
+  var HDKey = hdkey.fromMasterSeed(seed)
+  var examHDKey = HDKey.derivePath(path)
+  return examHDKey._hdkey._privateKey
+}
+
+var createWallet = function () {
+  var mnemonic = bip39.generateMnemonic()
+  var path = "m/44'/60'/0'/0/0" // first account
+  var seed = bip39.mnemonicToSeed(mnemonic)
+  var HDKey = hdkey.fromMasterSeed(seed)
+  var examHDKey = HDKey.derivePath(path)
+  return examHDKey._hdkey._privateKey
+}
+
+var transferFromWalletToWallet = function () {
+  var pvKey1 = loadGanacheWallet()
+  var pvKey2 = createWallet()
+  paymentInfo.pvKeys.sender = "0x" + pvKey1.toString('hex')
+  paymentInfo.pvKeys.receiver = "0x" + pvKey2.toString('hex')
+  payment(paymentInfo)
+}
+
+transferFromWalletToWallet()
