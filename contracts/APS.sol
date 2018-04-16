f18c7312fe88a8db352f700a6ec167b4b8925ed2
woong <wooong1225@gmail.com>
2018-04-16 16:54:03 -0700

    Add ERC20 API changes to address attacks

--- contracts/APS.sol
+++ contracts/APS.sol
@@ -6,7 +6,7 @@ contract APS{
     uint256 public decimals = 18;
     uint256 public totalSupply; 
     
-    mapping (address => uint) public balanceOf;
+    mapping (address => uint256) public balanceOf;
     mapping (address => mapping(address => uint256)) allowed;
 
     // Function that is called only once on creation
@@ -21,56 +21,72 @@ contract APS{
         balanceOf[msg.sender] = totalSupply;
     }
 
-    // Get the total token supply
-    function totalSupply() public constant returns (uint){
+    // ERC20 Standard: Get the total token supply
+    function totalSupply() public constant returns (uint256){
         return totalSupply;
     }
 
-    // Get the account balance of another account with address tokenOwner
-    function balanceOf(address tokenOwner) public constant returns (uint balance){
-        return balanceOf[tokenOwner];
+    // ERC20 Standard: Get the account balance of another account with address tokenOwner
+    function balanceOf(address _owner) public constant returns (uint256 balance){
+        return balanceOf[_owner];
     }
 
-    // Returns the amount which spender is still allowed to withdraw from tokenOwner
-    function allowance(address tokenOwner, address spender) public constant returns (uint remaining){
-        return allowed[tokenOwner][spender];
+    // ERC20 Standard: Returns the amount which spender is still allowed to withdraw from tokenOwner
+    function allowance(address _owner, address _spender) public constant returns (uint remaining){
+        return allowed[_owner][_spender];
     }
 
-    // Send tokens amount of tokens to address to
-    function transfer(address to, uint tokens) public returns (bool success){
-        //require(to != 0x0);
-        require(balanceOf[msg.sender]>= tokens); // check balanceOf msg.sender
-        require(balanceOf[to] + tokens >= balanceOf[to]); // prevent overflow
-        uint256 totalBalances = balanceOf[msg.sender] + balanceOf[to];
-        balanceOf[msg.sender] -= tokens;
-        balanceOf[to] += tokens;
-        emit Transfer(msg.sender,to,tokens);
-        assert(totalBalances == balanceOf[msg.sender] + balanceOf[to]);
+    function _transfer(address _from, address _to, uint256 _value) internal {
+        require(_to != 0x0);
+        require(balanceOf[_from]>= _value);
+        require(balanceOf[_to] + _value >= balanceOf[_to]);
+        uint256 totalBalances = balanceOf[_from] + balanceOf[_to];
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+        emit Transfer(_from,_to,_value);
+        assert(totalBalances == balanceOf[_from] + balanceOf[_to]);
+    }
+
+    // ERC20 Standard: Send tokens amount of tokens to address to
+    function transfer(address _to, uint256 _value) public returns (bool success){
+        _transfer(msg.sender,_to,_value);
         return true;
     }
 
-    // Allow spender to withdraw from your account, multiple times, up to the tokens amount. 
-    function approve(address spender, uint tokens) public returns (bool success){
-        require(spender != 0x0);
-        require(balanceOf[msg.sender]>=tokens);
-        allowed[msg.sender][spender] = tokens;
-        emit Approval(msg.sender,spender,tokens);
+    // ERC20 Standard: Allow spender to withdraw from your account, multiple times, up to the tokens amount. 
+    function approve(address _spender, uint256 _value) public returns (bool success){
+        require(_spender != 0x0);
+        require(balanceOf[msg.sender]>=_value);
+        allowed[msg.sender][_spender] = _value;
+        emit Approval(msg.sender,_spender,_value);
         return true;
     }
 
-    // send tokens from address from to address to
-    function transferFrom(address from, address to, uint tokens) public returns (bool success){
-        require(balanceOf[msg.sender]>=tokens);
-        require(allowed[msg.sender][from]>=tokens);
-        require(balanceOf[to] + tokens >= balanceOf[to]);
-        balanceOf[msg.sender] -= tokens;
-        balanceOf[to] += tokens;
-        allowed[msg.sender][from] -= tokens;
-        emit Transfer(msg.sender,to,tokens);
+    // current allowance for _spender is equalt to _currentValue, then overwrite it with _value and return true
+    // NOTE: Suggested ERC20 API changes to address attack
+    function approve(address _spender, uint256 _currentValue, uint256 _value) public returns (bool success){
+        require(_spender != 0x0);
+        require(balanceOf[msg.sender]>=_value);
+        require(allowed[msg.sender][_spender]== _currentValue);
+        allowed[msg.sender][_spender] = _value;
+        emit Approval(msg.sender,_spender,_currentValue,_value);
         return true;
     }
 
+    // ERC20 Standard: send tokens from address from to address to
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success){
+        require(allowed[_from][msg.sender]>=_value);
+        allowed[_from][msg.sender] -= _value;
+        _transfer(_from,_to,_value);
+        emit Transfer(msg.sender,_from,_to,_value);
+        return true;
+    }
 
-    event Transfer(address indexed from, address indexed to, uint tokens);
-    event Approval(address indexed tokenOwner, address indexed spender, uint tokens);    
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);    
+    // NOTE: Suggested ERC20 API changes to address attack, overloading events
+    event Transfer(address indexed _spender, address indexed _from, address indexed _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _oldValue, uint256 value);
+    
 }
+
