1677928a0a54a0b6b726573902cd852392ea1c36
GiWoongLee <wooong1225@gmail.com>
2018-05-01 14:26:30 -0700

    Refactor buy function

--- contracts/APS.sol
+++ contracts/APS.sol
@@ -12,8 +12,8 @@ contract APS{
     uint256 public decimals = 18;
     uint256 public totalSupply; 
     address public centralMinter; // Urbana
-    uint256 public buyPrice; // estimated in wei
-    uint256 public sellPrice; // estimated in wei
+    uint256 public buyPrice; 
+    uint256 public sellPrice; 
 
     mapping (address => uint256) public balanceOf;
     mapping (address => mapping(address => uint256)) allowed;
@@ -27,8 +27,8 @@ contract APS{
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
     event Approval(address indexed _owner, address indexed _spender, uint256 _value);    
     // NOTE: Suggested ERC20 API changes to address attack, overloading events
-    event Transfer(address indexed _spender, address indexed _from, address indexed _to, uint256 _value);
-    event Approval(address indexed _owner, address indexed _spender, uint256 _oldValue, uint256 value);
+    // event Transfer(address indexed _spender, address indexed _from, address indexed _to, uint256 _value);
+    // event Approval(address indexed _owner, address indexed _spender, uint256 _oldValue, uint256 value);
     
     // Function that is called only once on creation
     function APS(
@@ -40,9 +40,8 @@ contract APS{
         name = tokenName;
         symbol = tokenSymbol;
         totalSupply = initialSupply * 10 ** decimals;
-        balanceOf[msg.sender] = totalSupply;
+        balanceOf[msg.sender] = totalSupply; // TODO : decide protion, ex:50% of totalSupply
         centralMinter = msg.sender;
-        // if(tokenCentralMinter!=0x0) centralMinter = tokenCentralMinter; // Set Urbana as a owner on initialization
     }
 
     modifier onlyCentralMinter {
@@ -88,8 +87,7 @@ contract APS{
      * ERC20 Token functions
      */
     function buy() payable public returns (uint256 amount) {
-        // amount = uint256(msg.value / buyPrice); //TODO : Refactor division arithmetic, solidity not supporting floating point 
-        amount = msg.value; // Assuming 1 ether = 1 APS
+        amount = msg.value / buyPrice; 
         require(balanceOf[centralMinter]>= amount);
         balanceOf[msg.sender] += amount;
         balanceOf[centralMinter] -= amount;
@@ -129,7 +127,7 @@ contract APS{
     }
 
     // ERC20 Standard: Returns the amount which spender is still allowed to withdraw from tokenOwner
-    function allowance(address _owner, address _spender) public constant returns (uint remaining){
+    function allowance(address _owner, address _spender) public constant returns (uint256 remaining){
         return allowed[_owner][_spender];
     }
 
@@ -163,14 +161,14 @@ contract APS{
 
     // current allowance for _spender is equalt to _currentValue, then overwrite it with _value and return true
     // NOTE: Suggested ERC20 API changes to address attack
-    function approve(address _spender, uint256 _currentValue, uint256 _value) public returns (bool success){
-        require(_spender != 0x0);
-        require(balanceOf[msg.sender]>=_value);
-        require(allowed[msg.sender][_spender]== _currentValue);
-        allowed[msg.sender][_spender] = _value;
-        emit Approval(msg.sender,_spender,_currentValue,_value);
-        return true;
-    }
+    // function approve(address _spender, uint256 _currentValue, uint256 _value) public returns (bool success){
+    //     require(_spender != 0x0);
+    //     require(balanceOf[msg.sender]>=_value);
+    //     require(allowed[msg.sender][_spender]== _currentValue);
+    //     allowed[msg.sender][_spender] = _value;
+    //     emit Approval(msg.sender,_spender,_currentValue,_value);
+    //     return true;
+    // }
 
     // TODO : Find interface of relevant exchange to buy ethereum with cash. Ethereum would be used to buy APS.
     // added function approveAndCall to interact with etheruem exchange
@@ -187,7 +185,8 @@ contract APS{
         require(allowed[_from][msg.sender]>=_value);
         allowed[_from][msg.sender] -= _value;
         _transfer(_from,_to,_value);
-        emit Transfer(msg.sender,_from,_to,_value);
+        // emit Transfer(msg.sender,_from,_to,_value);
+        emit Transfer(_from,_to,_value);
         return true;
     }
 }
