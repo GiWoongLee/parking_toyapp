e10e1ce00edd972738b03ccd5716430c8431b8da
GiWoongLee <wooong1225@gmail.com>
2018-05-04 14:26:22 -0700

    Set truffle config file on various nets

--- truffle.js
+++ truffle.js
@@ -5,26 +5,42 @@ module.exports = {
       port : 8545,
       network_id : "*" // match any network id
     },
-    // ropsten : {
-    //   provider : function() {
-    //   return new HDWalletProvider(mnemonic,"https://ropsten.infura.io/");
-    // }
-    //   network_id : "3"
-    // },
-    // test : {
-    //   provider : function() {
-    //   return new HDWalletProvider(mnemonic, "https://127.0.0.1:8545/");
-    // }
-    //   network_id : "*",
-    // }
-    // live : {
-    //   host : "",
-    //   port : 80,
-    //   network_id : "1", // Ethereum public network
-    //   gas : 0,
-    //   gasPrice : 0,
-    //   from : "", // default address to use for any transaction Truffle makes during migrations
-    //   provide : "" // web3 provider instance Truffle should use to talk to the Ethereum network.
-    // }
+    private : {
+      host : "127.0.0.1",
+      port : 8545,
+      network_id : "",
+      from : "",
+      gas : 6712390
+    },
+    rinkeby : {
+      host : "127.0.0.1",
+      port : 8545,
+      network_id : "4", // Rinkeby ID 4
+      from : "",
+      gas : 6712390
+    },
+    ropsten : {
+      host : "127.0.0.1",
+      port : 8545,
+      network_id : "3",
+      from : "",
+      gas : 4500000
+    },
+    kovan : {
+      host : "127.0.0.1",
+      port : 9545,
+      network_id : "42",
+      from : "",
+      gas : 6712390
+    },
+    live : {
+      host : "127.0.0.1",
+      port : 80,
+      network_id : "1", // Ethereum public network
+      gas : 0,
+      gasPrice : 0,
+      from : "", // default address to use for any transaction Truffle makes during migrations
+      provide : "" // web3 provider instance Truffle should use to talk to the Ethereum network.
+    }
   }  
 };
